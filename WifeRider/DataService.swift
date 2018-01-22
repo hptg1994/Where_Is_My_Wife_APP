1be5d8877cd41b74e298cb61115c27ce6bb7b509
hptg1994 <hptg1994@gmail.com>
2018-01-22 11:01:22 -0500

    Add MapView and current Location

--- WifeRider/DataService.swift
+++ WifeRider/DataService.swift
@@ -8,18 +8,18 @@
 
 import Foundation
 import Firebase
-
 let DB_BASE = Database.database().reference()
 
 class DataService{
     static let instance = DataService()
-    
+
     private var _REF_BASE = DB_BASE
     private var _REF_USERS = DB_BASE.child("users")
     private var _REF_DRIVERS = DB_BASE.child("drivers")
     private var _REF_TRIPS = DB_BASE.child("trips")
     
     var REF_BASE : DatabaseReference{
+        
         return _REF_BASE
     }
     
--- WifeRider/HomeViewController.swift
+++ WifeRider/HomeViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 import RevealingSplashView
 
 class HomeViewController: UIViewController,MKMapViewDelegate{
@@ -17,29 +18,58 @@ class HomeViewController: UIViewController,MKMapViewDelegate{
     
     var delegate: CenterViewControllerDelegate?
     
+    var manager:CLLocationManager?
+    
+    var regionRadius:CLLocationDistance = 1000
+    
     let revealingSplashView = RevealingSplashView(iconImage: UIImage(named: "launchScreenIcon")!, iconInitialSize: CGSize(width: 80, height: 80), backgroundColor: UIColor.white)
-    
-    @IBAction func actionBtnWasPressed(_ sender: Any) {
-        actionBtn.animateButton(shouldLoad: true, withMessage: nil)
-       
-        
-        
-    }
-    
-    @IBAction func PressMenu(_ sender: Any) {
-        delegate?.toggleLeftPanel()
-        
-    }
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        manager = CLLocationManager()
+        manager?.delegate = self
+        manager?.desiredAccuracy = kCLLocationAccuracyBest
+        checkLocationAuthStatus()
         mapView.delegate = self
+        centerMapOnUserLocation()
         self.view.addSubview(revealingSplashView)
         revealingSplashView.animationType = SplashAnimationType.heartBeat
         revealingSplashView.startAnimation()
         revealingSplashView.heartAttack = true
     }
+    @IBAction func centerMapButton(_ sender: UIButton) {
+        centerMapOnUserLocation()
+    }
+    
+    @IBAction func actionBtnWasPressed(_ sender: Any) {
+        actionBtn.animateButton(shouldLoad: true, withMessage: nil)
+    }
+    
+    @IBAction func PressMenu(_ sender: Any) {
+        delegate?.toggleLeftPanel()
+    }
+    
+    func checkLocationAuthStatus(){
+        if CLLocationManager.authorizationStatus() == .authorizedAlways{
+            manager?.startUpdatingLocation()
+        }else{
+            manager?.requestAlwaysAuthorization()
+        }
+    }
+    
+    func centerMapOnUserLocation(){
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(mapView.userLocation.coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 }
 
+//这个相当于是告诉 Delegate 要干什么
+extension HomeViewController:CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthStatus()
+        if status == .authorizedAlways{
+            mapView.showsUserLocation = true
+            mapView.userTrackingMode = .follow
+        }
+    }
+}
